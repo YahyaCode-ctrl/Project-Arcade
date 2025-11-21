ee97119560834e7e1944e3733a9b490c9fb23711
YahyaCode-ctrl <yahyaelhadri@iesmontsia.org>
2025-11-21 15:46:20 +0100

    edit robot

--- robot.py
+++ robot.py
@@ -2,22 +2,11 @@
 import random
 
 def jugada_robot():
-    """
-    Retorna la jugada del robot per a Pedra, Paper, Tisora.
-    """
     opcions = ["pedra", "paper", "tisora"]
     
     return random.choice(opcions)
 
 def missatge_sortida():
-    """
-    Retorna un missatge aleatori per simular el timbre del robot.
-    """
-    missatges = [
-        "Beep-boop",
-        "Error 404: Trobada la teva jugada.",
-        "Timbre!",
-        "La meva CPU ja ho sabia."
-    ]
+    missatges = ["ets un noob", "traem un jugador millor que tu"]
     return random.choice(missatges)
 
